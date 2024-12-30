0f10388717470431bd2ff42498c3078b36ee26a3
wled-compile <138970516+wled-compile@users.noreply.github.com>
2024-12-30 12:07:16 +0100

    Add files via upload

--- do_hacks.py
+++ do_hacks.py
@@ -1,9 +1,13 @@
 import sys
 import re
 import json
+from datetime import datetime
 
 
-hacks_str=sys.argv[1]
+hacks_str=""
+sys.argv[1]
+if(len(sys.argv) > 1):
+    hacks_str=sys.argv[1]
 dataset={}
 
 with open('dataset.json') as f:
@@ -24,3 +28,10 @@ for id_str in hacks_str.split(";"):
     except Exception:
         print("cannot apply hack with id="+id_str)
         pass
+
+try:
+    with open("wled00/data/settings_sec.htm", 'r') as file:
+        data=file.read()
+        data = data.replace("##VERSION##", '##VERSION##<br>Compiled using <a href="https://wled-compile.github.io/" target="_blank">online wled compiler</a> provided by <a href="https://wshop.myhome-control.de" target="_blank">MyHome-Control</a><br>(compile UTC date/time: '+datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+')')
+    with open(hack["file"], 'w') as file:
+        file.write(data)
